21e424f9efc07cd8efdc8afd2cb8230d661b8d37
Rais Ikhsan <93347953+FoodyRice@users.noreply.github.com>
2022-01-07 09:27:34 -0500

    Create FoodyRice.plugin.js

---
+++ FoodyRice.plugin.js
@@ -0,0 +1,7 @@
+/**
+ * @name Discord Tweaks
+ * @description Quality Of Life Changes To Discord
+ * @author FoodyRice
+ * @version 69.420
+ * @authorLink https://www.youtube.com/channel/UC8OAmRGcLAURuAJSSD1lDPg
+*/
